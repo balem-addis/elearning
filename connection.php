8d6092f085397fb6bf392cfd00ad48c47507a92a
Bantegize Addis Alemayehu <acabuchane@gmail.com>
2021-06-21 14:55:42 +0300

    change permission and use mysqli function on connection

--- connection.php
+++ connection.php
@@ -3,14 +3,14 @@ define("db_server","localhost");
 define("db_user","root");
 define("db_name","e-learning");
 define("db_password","");
-$conn=mysql_connect(db_server,db_user);
+$conn=mysqli_connect(db_server,db_user,db_password,db_name);
 if(!$conn)
 {
-die("Error in connection".mysql_error());
-}
+die("Error in connection".mysqli_error());
+}/*
 $db_select=mysql_select_db(db_name,$conn);
 if(!$db_select)
 {
 die("Error in db select".mysql_error());
-}
+}*/
 ?>
