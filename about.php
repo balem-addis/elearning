b648343270494ea8ac2a1e131010c272aee55254
Balemlay Addis <balemlayaddis3@gmail.com>
2021-06-23 06:14:25 -0700

    modified files

--- about.php
+++ about.php
@@ -88,7 +88,7 @@ timeimgs(numb = ++numb);
 <br><br>
 <table border="0"  width="1210"height="450" align="center" >
 <tr ><td width="210" align="center" valign="center" ><font  color="white" ><b><br>
- DMCTE ELMS</b> </font><BR>
+ BICHENA TVET ELMS</b> </font><BR>
 <img src="images/e-learning.jpg"  width="250" height="250"></td>
 <td width="700" height="300" rowspan=4 align="center"valign="top" class="one"  bgcolor="ffffff"><div><p><center>
    <h3>&nbsp;</h3>
@@ -100,7 +100,7 @@ timeimgs(numb = ++numb);
    <h4>1.1.	 Background of the project &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-Debre markos college of Teachers education is located in Debre markos city of Amhara Regional state, which is 300kms north of Addis Ababa. The college opened in 1994 E.C. The main objective of the college is to provide teachers and education experts capable of building up citizens by shaping the generation with knowledge, morality, ability and skills, feeling national responsibilities, enjoying positive human and democratic outlook, having developed research capacity, standing for the quality and development of education and equipped with ethical values of teaching and other related professions committed to serving the society to the best of their capacity and ability. Regarding this the teaching learning process takes the key position and learning process needs techniques and tools to present the knowledge (from different resources) interact with it and share it with others. In this context, E-Learning is becoming an important tool to support the learning system to achieve its goal. E-Learning became hot topic in the 1990's after the spread of the Internet. 
+BICHENA college of Teachers education is located in Debre markos city of Amhara Regional state, which is 3265kms north of Addis Ababa. The college opened in 1994 E.C. The main objective of the college is to provide teachers and education experts capable of building up citizens by shaping the generation with knowledge, morality, ability and skills, feeling national responsibilities, enjoying positive human and democratic outlook, having developed research capacity, standing for the quality and development of education and equipped with ethical values of teaching and other related professions committed to serving the society to the best of their capacity and ability. Regarding this the teaching learning process takes the key position and learning process needs techniques and tools to present the knowledge (from different resources) interact with it and share it with others. In this context, E-Learning is becoming an important tool to support the learning system to achieve its goal. E-Learning became hot topic in the 1990's after the spread of the Internet. 
 </h4>
  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
 
